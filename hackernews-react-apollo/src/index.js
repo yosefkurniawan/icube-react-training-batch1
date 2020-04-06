52f06b8c878c50f94d52bef5f7275e4a79b3773b
yosefkurniawan <jojo@icube.us>
2020-04-06 14:22:25 +0700

    Apollo training - implement login/signup mutation and store token into localstorage

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -5,24 +5,31 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
-// 1
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
 
-// 2
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    };
+});
 const httpLink = createHttpLink({
     uri: "http://localhost:4000",
 });
 
-// 3
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
-// 4
 ReactDOM.render(
     <BrowserRouter>
         <ApolloProvider client={client}>

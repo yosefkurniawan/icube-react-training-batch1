d2273e16d4f1ced045ccd868a5974dcfdf37c2cf
yosefkurniawan <jojo@icube.us>
2020-04-06 18:15:09 +0700

    Apollo training - implement subscription on new link

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -27,6 +27,27 @@ export const FEED_QUERY = gql`
     }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
 class LinkList extends Component {
     _updateCacheAfterVote = (store, createVote, linkId) => {
         const data = store.readQuery({ query: FEED_QUERY });
@@ -37,14 +58,38 @@ class LinkList extends Component {
         // console.log(createVote);
         // console.log(linkId);
         store.writeQuery({ query: FEED_QUERY, data });
-    }
-    render() {
+    };
 
+    _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+                const newLink = subscriptionData.data.newLink;
+                const exists = prev.feed.links.find(
+                    ({ id }) => id === newLink.id
+                );
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [...prev.feed.links, newLink],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename,
+                    },
+                });
+            },
+        });
+    };
+
+    render() {
         return (
             <Query query={FEED_QUERY}>
-                {({ data, loading, error }) => {
+                {({ loading, error, data, subscribeToMore }) => {
                     if (loading) return <div>Loading...</div>;
                     if (error) return <div>Error!</div>;
+
+                    this._subscribeToNewLinks(subscribeToMore);
 
                     const LinksToRender = data.feed.links;
 

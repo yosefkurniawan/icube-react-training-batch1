eb59cdf688ca9a42739b242854cf1e8592ff70dc
yosefkurniawan <jojo@icube.us>
2020-04-25 15:53:53 +0700

    learn formik

---
+++ formik-test/src/App.form1.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+function App() {
+    return (
+        <>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = 'Required!';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                            values.email
+                        )
+                    ) {
+                        errors.email = 'Email is invalid!';
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 500);
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                        />
+                        {errors.email && touched.email && errors.email}
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                        />
+                        {errors.password && touched.password && errors.password}
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )}
+            </Formik>
+        </>
+    );
+}
+
+export default App;
---
+++ formik-test/src/App.form2.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+function App() {
+    return (
+        <>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = 'Required!';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                            values.email
+                        )
+                    ) {
+                        errors.email = 'Email is invalid!';
+                    }
+
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 500);
+                }}
+            >
+                {({
+                    handleSubmit,
+                    isSubmitting,
+                }) => (
+                    <Form>
+                        <Field type="email" name="email" />
+                        <ErrorMessage name="email" component="div" />
+
+                        <Field type="password" name="password" />
+                        <ErrorMessage name="password" component="div" />
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </>
+    );
+}
+
+export default App;

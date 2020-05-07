014214bd39fee821a8da5502b64d306d6a161dd6
yosefkurniawan <jojo@icube.us>
2020-05-07 14:27:25 +0700

    fix state

--- with-apollo-and-redux-app/redux/reducerCart.js
+++ with-apollo-and-redux-app/redux/reducerCart.js
@@ -1,29 +1,28 @@
-const initState = {
-    cart: [
-        // {
-        //     name: 'Test Configurable Product',
-        //     image:
-        //         'https://swiftpwa-be.testingnow.me/media/catalog/product/cache/8f52cebd15478e5e402d55b72e1f5d5d/m/h/mh01-gray_main_2_1.jpg',
-        //     price: { currency: 'USD', value: 45, __typename: 'Money' },
-        //     qty: '2',
-        //     sku: 'test-configurable',
-        // },
-        // {
-        //     name: 'Test 2',
-        //     image:
-        //         'https://swiftpwa-be.testingnow.me/media/catalog/product/cache/8f52cebd15478e5e402d55b72e1f5d5d/m/h/mh01-gray_main_2_1.jpg',
-        //     price: { currency: 'USD', value: 50, __typename: 'Money' },
-        //     qty: '3',
-        //     sku: 'test-2',
-        // },
-    ],
-};
+const initState = [
+    {
+        name: 'Test Configurable Product',
+        image:
+            'https://swiftpwa-be.testingnow.me/media/catalog/product/cache/8f52cebd15478e5e402d55b72e1f5d5d/m/h/mh01-gray_main_2_1.jpg',
+        price: { currency: 'USD', value: 45, __typename: 'Money' },
+        qty: '2',
+        sku: 'test-configurable',
+    },
+    {
+        name: 'Test 2',
+        image:
+            'https://swiftpwa-be.testingnow.me/media/catalog/product/cache/8f52cebd15478e5e402d55b72e1f5d5d/m/h/mh01-gray_main_2_1.jpg',
+        price: { currency: 'USD', value: 50, __typename: 'Money' },
+        qty: '3',
+        sku: 'test-2',
+    },
+]
 
 export function cart(state = initState, action) {
     switch (action.type) {
         case 'ADD_TO_CART':
             return [...state, action.item];
+            // return state;
         default:
-            return state
+            return state;
     }
 }
--- with-apollo-and-redux-app/redux/store.js
+++ with-apollo-and-redux-app/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import reduxLogger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { cart } from './reducerCart';
 
 export const initializeStore = () => {
     const middlewares = applyMiddleware(reduxLogger);
-    return createStore(cart, composeWithDevTools(middlewares));
+    const reducers = combineReducers({cart});
+    return createStore(reducers, composeWithDevTools(middlewares));
 }
